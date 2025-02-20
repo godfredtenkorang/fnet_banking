a7bdd75677e9b4e85cc4982716871e36c35a401f
benjamin <benjaminoheneasare65@gmail.com>
2025-02-20 12:16:00 -0800

    changes

--- agent/views.py
+++ agent/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def agent_dashboard(request):
     return render(request, 'agent/dashboard.html')
+
+def cashIn(request):
+    return render(request, 'agent/cashIn.html')
+
+def cashOut(request):
+    return render(request, 'agent/cashOut.html')
