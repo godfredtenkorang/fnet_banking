875656abe288b7acd12061df163b1fbc260f2b77
benjamin <benjaminoheneasare65@gmail.com>
2025-02-22 15:11:30 -0800

    change

--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def registerAgent(request):
+    return render(request, 'owner/registerAgent.html')
+
+def myAgent(request):
+    return render(request, 'owner/myAgent.html')
