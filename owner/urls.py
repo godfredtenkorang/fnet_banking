191fee74bd0d2fefd7b79991b5237f419e8c8cf8
benjamin <benjaminoheneasare65@gmail.com>
2025-02-22 16:00:02 -0800

    changes

--- owner/urls.py
+++ owner/urls.py
@@ -4,4 +4,16 @@ from . import views
 urlpatterns = [
  path('registerAgent/', views.registerAgent, name='registerAgent'),
  path('myAgent/', views.myAgent, name='myAgent'),
+
+ path('report/', views.report, name='report'),
+path('payto/', views.payto, name='payto'),
+    path('users/', views.users, name='users'),
+    path('register_customer/', views.register_customer, name='register_customer'),
+    path('flot_resources/', views.flot_resources, name='flot_resources'),
+    path('agent_accounts/', views.agent_accounts, name='agent_accounts'),
+    path('bank_account/', views.bank_account, name='bank_account'),
+    path('bank_linkage/', views.bank_linkage, name='bank_linkage'),
+    path('customer_care/', views.customer_care, name='customer_care'),
+        path('financial-services/', views.financial_services, name='financial_services'),
+
 ]
--- owner/views.py
+++ owner/views.py
@@ -7,3 +7,35 @@ def registerAgent(request):
 
 def myAgent(request):
     return render(request, 'owner/myAgent.html')
+
+def report(request):
+    return render(request, 'owner/report.html')
+
+
+def payto(request):
+    return render(request, 'owner/payto.html')
+
+def users(request):
+    return render(request, 'owner/users.html')
+
+def register_customer(request):
+    return render(request, 'owner/register_customer.html')
+
+def flot_resources(request):
+    return render(request, 'owner/flot_resources.html')
+
+def agent_accounts(request):
+    return render(request, 'owner/agent_accounts.html')
+
+def bank_account(request):
+    return render(request, 'owner/bank_account.html')
+
+def bank_linkage(request):
+    return render(request, 'owner/bank_linkage.html')
+
+def customer_care(request):
+    return render(request, 'owner/customer_care.html')
+
+
+def financial_services(request):
+    return render(request, 'owner/FinancialServices.html')
