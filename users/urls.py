ff967a19edabf63419b5a57eb058f495fe2637b5
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-05-04 22:39:05 +0000

    changes

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import LoginView
 
 urlpatterns = [
@@ -40,6 +41,7 @@ urlpatterns = [
     
     # API
      path('login/', LoginView.as_view(), name='user_login'),
+     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
      
     
 ]
