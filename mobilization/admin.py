7d164653a0d357c738384fd6fe31ae22304cb4b4
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-07-01 13:53:05 +0000

    changes

--- mobilization/admin.py
+++ mobilization/admin.py
@@ -6,6 +6,14 @@ admin.site.register(MobilizationPayTo)
 admin.site.register(BankDeposit)
 admin.site.register(BankWithdrawal)
 admin.site.register(PaymentRequest)
-admin.site.register(CustomerAccount)
+@admin.register(CustomerAccount)
+class CustomerAccountAdmin(admin.ModelAdmin):
+    
+    list_display = ('customer', 'account_number', 'account_name', 'bank', 'phone_number', 'date_added')
+    list_filter = ('phone_number', 'account_number')
+    search_fields = ('phone_number', 'account_number', 'account_name')
+    
+    
+    
 admin.site.register(TellerCalculator)
 admin.site.register(Report)
